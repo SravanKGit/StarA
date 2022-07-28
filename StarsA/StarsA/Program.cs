6a3479484d316f45b640b1114e4ab6528924aa08
Sravan Yadav <ksravanyadav@outlook.com>
2022-07-28 17:01:31 +0530

    Initial push

---
+++ StarsA/StarsA/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StarsA
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter number of rows\n");
+            var number = int.Parse(Console.ReadLine());
+            for (int row = 1; row <= number; ++row)
+            {
+                for (int col = 1; col <= row; ++col)
+                {
+                    Console.Write(" * ");
+                }
+
+                Console.WriteLine();
+            }
+            Console.ReadLine();
+        }
+    }
+}
